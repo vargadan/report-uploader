e12ccdff23588c1f9e5186fb1b71aca772da93a3
vargadan <danielvrg@gmail.com>
2018-02-14 01:17:19 +0100

    x

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,49 +1,45 @@
 node('maven') {
+	def APP_NAME = "report-uploader"
    	// define commands
    	def mvnCmd = "mvn -s configuration/maven-cicd-settings.xml"
+   	def CICD_PROJECT = "reportengine-cicd"
    	def DEV_PROJECT = "reportengine-dev"
-   	def IT_PROJECT = "reportengine-it"
+   	def QA_PROJECT = "reportengine-qa"
+   	def PROD_PROJECT = "reportengine-prod"
    	def PORT = 8080
-   	def APP_NAME = "report-uploader"
-
-   	stage ('Build') {
-   		git branch: 'master', url: 'https://github.com/vargadan/report-uploader.git'
-   		sh "${mvnCmd} clean package -DskipTests=true"
+   	def GIT_URL = "https://github.com/vargadan/${APP_NAME}.git"
+   	def SKIP_TEST = "false"
+ 
+   	stage ('Build & Test') {
+   		git branch: 'master', url: "${GIT_URL}"
+   		sh "${mvnCmd} clean package -DskipTests=${SKIP_TEST} fabric8:build"
    	}
    	
    	def version = version()
    	
-   	stage ('Test') {
-   		sh "${mvnCmd} test"
-   		step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-   	}
-
-   	stage ('Deploy DEV') {
-	   // clean up. keep the image stream
-	   sh "oc project ${DEV_PROJECT}"
-	   sh "oc delete buildconfigs,deploymentconfigs,services,routes -l app=${APP_NAME} -n ${DEV_PROJECT}"
-	   // create build. override the exit code since it complains about exising imagestream
-	   sh "${mvnCmd} fabric8:deploy -DskipTests"
+	stage ('Deploy DEV') {
+	   	// create build. override the exit code since it complains about exising imagestream
+	   	//tag for version in DEV imagestream
+	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${CICD_PROJECT}/${APP_NAME}:${version}"
+	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${DEV_PROJECT}/${APP_NAME}:latest"
+		envSetup(DEV_PROJECT, APP_NAME, 'latest')
 	}
 
-   stage ('Deploy IT') {
-     	timeout(time:5, unit:'MINUTES') {
-        		input message: "Promote to IT?", ok: "Promote"
+   	stage ('Deploy to QA') {
+     	timeout(time:10, unit:'MINUTES') {
+        		input message: "Promote to QA?", ok: "Promote"
         }
-	   sh "oc project ${IT_PROJECT}"
-	   // tag for stage
-	   sh "oc tag ${DEV_PROJECT}/${APP_NAME}:latest ${IT_PROJECT}/${APP_NAME}:${version}"
-	   // clean up. keep the imagestream
-	   sh "oc delete buildconfigs,deploymentconfigs,services,routes -l app=${APP_NAME} -n ${IT_PROJECT}"
-	   // deploy stage image
-	   sh "oc new-app ${APP_NAME}:${version} -n ${IT_PROJECT}" 
-	   // delete service and route because new-app created them with wrong port
-	   sh "oc delete svc,route -l app=${APP_NAME} -n ${IT_PROJECT}"
-	   // create service with the right port 
-	   sh "oc expose dc ${APP_NAME} --port=${PORT}"
-	   // create route with the right port
-	   sh "oc expose svc ${APP_NAME}"
+        //put into QA imagestream
+        sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${QA_PROJECT}/${APP_NAME}:latest"
+        envSetup(QA_PROJECT, APP_NAME, 'latest')
 	}
+
+}
+
+def envSetup(project, appName, version) {
+	sh "oc delete buildconfig,deploymentconfig,service,routes -l app=${appName} -n ${project}"
+   	sh "oc new-app ${appName}:${version} -n ${project}"
+   	sh "oc expose svc ${appName} -n ${project}"
 }
 
 def version() {
