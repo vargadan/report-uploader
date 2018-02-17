e31048b545a03dbd7245d6bb8fbe2f01dfa574ec
vargadan <danielvrg@gmail.com>
2018-02-17 18:24:14 +0100

    x

--- pipeline.groovy
+++ pipeline.groovy
@@ -22,38 +22,42 @@ node('maven') {
 	   	//tag for version in DEV imagestream
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${CICD_PROJECT}/${APP_NAME}:${version}"
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${DEV_PROJECT}/${APP_NAME}:latest"
-		envSetup(DEV_PROJECT, APP_NAME, 'latest', true)
+		envSetup(DEV_PROJECT, APP_NAME, 'latest', PORT, true)
 	}
 
    	stage ('Deploy to IT') {
         //put into IT imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${IT_PROJECT}/${APP_NAME}:latest"
-        envSetup(IT_PROJECT, APP_NAME, 'latest', true)
+        envSetup(IT_PROJECT, APP_NAME, 'latest', PORT, true)
 	}
 	
    	stage ('Deploy to PROD') {
         //put into PROD imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${PROD_PROJECT}/${APP_NAME}:latest"
-        envSetup(PROD_PROJECT, APP_NAME, 'latest', false)
+        envSetup(PROD_PROJECT, APP_NAME, 'latest', PORT, false)
 	}
 
 }
 
-def envSetup(project, appName, version, recreate) {
+def envSetup(project, appName, version, port, recreate) {
 	GET_DC_OUT = sh (
 		script: "oc get deploymentconfig -l app=${appName} -n ${project}",
 		returnStdout: true
 	).trim()
 	echo "GET_DC_OUT : ${GET_DC_OUT}"
 	appExists = GET_DC_OUT.contains(appName)
-	if (appExists && recreate) {
+	if (appExists && !recreate) {
+		//since the app exists and we dont recreate it we can exit
+		return
+	} else if (appExists && recreate) {
+		//we can delete the app if we want to recreate
 		sh "oc delete deploymentconfig,service,routes -l app=${appName} -n ${project}"
-		sh "oc new-app ${appName}:${version} -n ${project}"
-   		sh "oc expose svc ${appName} -n ${project}"
-   	} else if (!appExists) {
- 		sh "oc new-app ${appName}:${version} -n ${project}"
-   		sh "oc expose svc ${appName} -n ${project}"
-   	}
+	}
+	//now we can create the app since it has either been deleted or it did not exist at all
+ 	sh "oc new-app ${appName}:${version} -n ${project}"
+   	sh "oc delete service,routes -l app=${appName} -n ${project}"
+   	sh "oc create service clusterip dummy-report-factory --tcp=${port}:${port}"
+   	sh "oc expose service dummy-report-factory"	
 }
 
 def version() {
