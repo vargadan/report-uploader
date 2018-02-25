af95eeef8a19d66277f101c727cfd06fa70893e7
vargadan <danielvrg@gmail.com>
2018-02-25 17:48:35 +0100

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -22,42 +22,18 @@ node('maven') {
 	   	//tag for version in DEV imagestream
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${CICD_PROJECT}/${APP_NAME}:${version}"
 	   	sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${DEV_PROJECT}/${APP_NAME}:latest"
-		envSetup(DEV_PROJECT, APP_NAME, 'latest', PORT, true)
 	}
 
    	stage ('Deploy to IT') {
         //put into IT imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${IT_PROJECT}/${APP_NAME}:latest"
-        envSetup(IT_PROJECT, APP_NAME, 'latest', PORT, true)
 	}
 	
    	stage ('Deploy to PROD') {
         //put into PROD imagestream
         sh "oc tag ${CICD_PROJECT}/${APP_NAME}:latest ${PROD_PROJECT}/${APP_NAME}:latest"
-        envSetup(PROD_PROJECT, APP_NAME, 'latest', PORT, false)
 	}
 
-}
-
-def envSetup(project, appName, version, port, recreate) {
-	GET_DC_OUT = sh (
-		script: "oc get deploymentconfig -l app=${appName} -n ${project}",
-		returnStdout: true
-	).trim()
-	echo "GET_DC_OUT : ${GET_DC_OUT}"
-	appExists = GET_DC_OUT.contains(appName)
-	if (appExists && !recreate) {
-		//since the app exists and we dont recreate it we can exit
-		return
-	} else if (appExists && recreate) {
-		//we can delete the app if we want to recreate
-		sh "oc delete deploymentconfig,service,routes -l app=${appName} -n ${project}"
-	}
-	//now we can create the app since it has either been deleted or it did not exist at all
- 	sh "oc new-app ${appName}:${version} -n ${project}"
-   	sh "oc delete service,routes -l app=${appName} -n ${project}"
-   	sh "oc create service clusterip ${appName} --tcp=${port}:${port} -n ${project}"
-   	sh "oc expose service ${appName} -n ${project}"	
 }
 
 def version() {
